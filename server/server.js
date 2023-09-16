022558faaf6d86b4d847c443994e76d62c6303ce
Steven-Jarmell <sjj27@pitt.edu>
2023-09-16 01:16:39 -0400

    Remove second listen

--- server/server.js
+++ server/server.js
@@ -8,18 +8,9 @@ app.get('/', (req, res)=>{
     res.status(200);
     res.send("Welcome to root URL of Server");
 });
-  
-app.listen(PORT, (error) =>{
-    if(!error)
-        console.log("Server is Successfully Running, and App is listening on port "+ PORT)
-    else 
-        console.log("Error occurred, server can't start", error);
-    }
-);
 
 const start = async() => {
-    await mongoose.connect('');
-
+    await mongoose.connect('SECRET');
     app.listen(PORT, (error) =>{
         if(!error)
             console.log("Server is Successfully Running, and App is listening on port "+ PORT)
