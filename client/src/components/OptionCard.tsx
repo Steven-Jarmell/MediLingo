de130dd9360cdd560ff103229f60565ba31b4a9a
Steven-Jarmell <sjj27@pitt.edu>
2023-09-16 14:44:06 -0400

    Fix navigate path

--- client/src/components/OptionCard.tsx
+++ client/src/components/OptionCard.tsx
@@ -10,7 +10,7 @@ type Props = {
 const OptionCard = ({ name, path, imgSrc }: Props) => {
     const nav = useNavigate();
     return (
-        <div className="option-card__container" onClick={() => nav('/home' + path)}>
+        <div className="option-card__container" onClick={() => nav('/home/path' + path)}>
             <img src={imgSrc} alt={`${name} icon`} className="option-card__icon" />
             <span className="option-card__name">{name}</span>
         </div>
