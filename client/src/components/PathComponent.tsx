2dcdcc26be707970343cbf46db06edce13b6f221
JohnRob1 <robin557@purdue.edu>
2023-09-16 15:52:28 -0400

    node-fix

--- client/src/components/PathComponent.tsx
+++ client/src/components/PathComponent.tsx
@@ -1,6 +1,5 @@
-import { useNavigate, useParams } from "react-router-dom";
-import ReactFlow from 'react-flow-renderer';
-import CustomNode from "./CustomNode";
+import { useNavigate } from "react-router-dom";
+import ReactFlow from "react-flow-renderer";
 
 const w = window.innerWidth;
 const h = window.innerHeight;
@@ -8,24 +7,41 @@ const h = window.innerHeight;
 console.log("w: " + w);
 console.log("h: " + h);
 
-const nodeTypes = {
-    custom: CustomNode,
-}
-
-const initialNodes = [
-    { id: '1', type: 'custom', position: {x: w/2-50, y:0}, data: { label: 'Asthma' } },
-    { id: '2', type: 'custom', position: {x: w*(1/8)-75, y: h/8}, data: { label: 'Allergenic Asthma' } },
-    { id: '3', type: 'custom', position: {x: w*(3/8)-75, y: h/8}, data: { label: 'Non-Allergenic Asthma' } },
-    { id: '4', type: 'custom', position: {x: w*(5/8)-50, y: h/8}, data: { label: 'Steroid-Resistant Asthma' } },
-    { id: '5', type: 'custom', position: {x: w*(7/8)-50, y: h/8}, data: { label: 'Exercise-Induced Asthma' } },
-];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2', animated: true},
-                        { id: 'e1-3', source: '1', target: '3', animated: true },
-                        { id: 'e1-4', source: '1', target: '4', animated: true },
-                        { id: 'e1-5', source: '1', target: '5', animated: true }];
+const initialEdges = [{ id: 'e1-2', source: '0', target: '1', animated: true},
+                        { id: 'e1-3', source: '0', target: '2', animated: true },
+                        { id: 'e1-4', source: '0', target: '3', animated: true },
+                        { id: 'e1-5', source: '0', target: '4', animated: true }];
 
 const PathComponent = () => {
-    // const navigate = useNavigate();
+
+    const navigate = useNavigate();
+
+    const initialNodes = [
+        { id: '0', position: {x: w/2-50, y:0}, data: { label: 'Asthma' }},
+        { id: '1', position: {x: w*(1/8)-75, y: h/8}, data: { label: 'Allergenic Asthma'} },
+        { id: '2', position: {x: w*(3/8)-75, y: h/8}, data: { label: 'Non-Allergenic Asthma'} },
+        { id: '3', position: {x: w*(5/8)-50, y: h/8}, data: { label: 'Steroid-Resistant Asthma'} },
+        { id: '4', position: {x: w*(7/8)-50, y: h/8}, data: { label: 'Exercise-Induced Asthma'} },
+    ];
+
+    const handleNodeClick = (event, node) => {
+        navigate("../../lesson/" + node.id);
+      };
+
+    return (
+        <div className="flex justify-center w-screen h-screen">
+            <ReactFlow 
+                nodes={initialNodes} 
+                edges={initialEdges} 
+                onNodeClick={handleNodeClick}
+                />
+        </div>
+    );
+  
+}
+export default PathComponent;
+
+// const navigate = useNavigate();
 
 //   type questionProps = {
 //     questionText: string;
@@ -71,16 +87,3 @@ const PathComponent = () => {
 
 // };
 //   const { condition } = useParams();
-
-    return (
-      <div className="flex justify-center w-screen h-screen">
-        <ReactFlow 
-            nodeTypes={nodeTypes}
-            nodes={initialNodes} 
-            edges={initialEdges} 
-            />
-      </div>
-    );
-  
-}
-export default PathComponent;
