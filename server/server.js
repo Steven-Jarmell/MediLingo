19935633263c017a4baf0bff04790781bcdb16f4
David <davidliao9310@gmail.com>
2023-09-16 01:33:54 -0400

    Revert "fix function cuz sleepy at 1am"
    This reverts commit 4c5f73ed1eff836901bf8e56128dd9d09a2921fa.

--- server/server.js
+++ server/server.js
@@ -10,12 +10,7 @@ app.get('/', (req, res)=>{
 });
 
 const start = async() => {
-<<<<<<< HEAD
     await mongoose.connect('SECRET');
-=======
-    await mongoose.connect('mongodb+srv://boss:boss@pitt2023.61dxycp.mongodb.net/?retryWrites=true&w=majority');
-
->>>>>>> 109cc47 (fix function cuz sleepy at 1am)
     app.listen(PORT, (error) =>{
         if(!error)
             console.log("Server is Successfully Running, and App is listening on port "+ PORT)
