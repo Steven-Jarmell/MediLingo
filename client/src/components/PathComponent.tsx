64bdd8847e0fb2bb12a8dddb572327692b02a7e1
Steven-Jarmell <sjj27@pitt.edu>
2023-09-16 21:46:44 -0400

    Add clarity to paths

--- client/src/components/PathComponent.tsx
+++ client/src/components/PathComponent.tsx
@@ -2,7 +2,7 @@
 
 import { useNavigate } from "react-router-dom";
 import ReactFlow from "react-flow-renderer";
-import "../css/Trees.css"
+import "../css/Trees.css";
 
 const w = window.innerWidth;
 const h = window.innerHeight;
@@ -10,106 +10,93 @@ const h = window.innerHeight;
 console.log("w: " + w);
 console.log("h: " + h);
 
-const initialEdges = [{ id: 'e0-1', source: '0', target: '1', animated: true },
-                        { id: 'e0-2', source: '0', target: '2', animated: true },
-                        { id: 'e0-3', source: '0', target: '3', animated: true },
-                        { id: 'e0-4', source: '0', target: '4', animated: true },
-                        { id: 'e1-5', source: '1', target: '5', animated: true },
-                        { id: 'e2-5', source: '2', target: '5', animated: true },
-                        { id: 'e3-5', source: '3', target: '5', animated: true },
-                        { id: 'e4-5', source: '4', target: '5', animated: true }];
+const initialEdges = [
+    { id: "e0-1", source: "0", target: "1", animated: true },
+    { id: "e0-2", source: "0", target: "2", animated: true },
+    { id: "e0-3", source: "0", target: "3", animated: true },
+    { id: "e0-4", source: "0", target: "4", animated: true },
+    { id: "e1-5", source: "1", target: "5", animated: true },
+    { id: "e2-5", source: "2", target: "5", animated: true },
+    { id: "e3-5", source: "3", target: "5", animated: true },
+    { id: "e4-5", source: "4", target: "5", animated: true },
+];
 
 const PathComponent = () => {
-
     const initialNodes = [
-        { id: '0', position: {x: w/2-150, y:0}, data: { label: 'Asthma' }, className: "tree_node" },
-        { id: '1', position: {x: w*(1/8)-150, y: h/8}, data: { label: 'Allergenic Asthma'}, className: "tree_node"},
-        { id: '2', position: {x: w*(3/8)-150, y: h/8}, data: { label: 'Non-Allergenic Asthma'}, className: "tree_node" },
-        { id: '3', position: {x: w*(5/8)-100, y: h/8}, data: { label: 'Occupational Asthma'}, className: "tree_node" },
-        { id: '4', position: {x: w*(7/8)-100, y: h/8}, data: { label: 'Exercise-Induced Asthma'}, className: "tree_node" },
-        { id: '5', position: {x: w/2-200, y: h/2}, data: { label: 'Resources'}, className: "resources_node" },
+        {
+            id: "0",
+            position: { x: w / 2 - 150, y: 0 },
+            data: { label: "Asthma" },
+            className: "tree_node",
+        },
+        {
+            id: "1",
+            position: { x: w * (1 / 8) - 150, y: h / 8 },
+            data: { label: "Allergenic Asthma" },
+            className: "tree_node",
+        },
+        {
+            id: "2",
+            position: { x: w * (3 / 8) - 150, y: h / 8 },
+            data: { label: "Non-Allergenic Asthma" },
+            className: "tree_node",
+        },
+        {
+            id: "3",
+            position: { x: w * (5 / 8) - 100, y: h / 8 },
+            data: { label: "Occupational Asthma" },
+            className: "tree_node",
+        },
+        {
+            id: "4",
+            position: { x: w * (7 / 8) - 100, y: h / 8 },
+            data: { label: "Exercise-Induced Asthma" },
+            className: "tree_node",
+        },
+        {
+            id: "5",
+            position: { x: w / 2 - 200, y: h / 2 },
+            data: { label: "Resources" },
+            className: "resources_node",
+        },
     ];
 
     const navigate = useNavigate();
 
     const handleNodeClick = (event, node) => {
-        if (node.id === '5') {
+        if (node.id === "5") {
             navigate("../../research");
-        }
-        else {
+        } else {
             navigate("../../lesson/" + node.id);
         }
     };
-    
-    const completed_lessons = localStorage.getItem('lessons-completed')?.split(';');
-    initialNodes.forEach(element => {
-        if(completed_lessons?.includes(element.id)) {
-            element.data.label = element.data.label + ' Completed!' 
+
+    const completed_lessons = localStorage
+        .getItem("lessons-completed")
+        ?.split(";");
+    initialNodes.forEach((element) => {
+        if (completed_lessons?.includes(element.id)) {
+            element.data.label = element.data.label + " Completed!";
             element.className = "completed_tree_node";
-            initialEdges.forEach(edge => {
-                if(element.id === edge.id[edge.id.length - 1]) {
+            initialEdges.forEach((edge) => {
+                if (element.id === edge.id[edge.id.length - 1]) {
                     edge.animated = false;
                 }
-            })
+            });
         }
     });
 
-  
     return (
-        <div className="flex justify-center w-screen h-screen">
-            <ReactFlow 
-                nodes={initialNodes} 
-                edges={initialEdges} 
-                onNodeClick={handleNodeClick}
+        <div className="flex flex-col">
+            <span className="text-center text-3xl mb-6">Click on Asthma to learn general Asthma information, then continue on to other paths!</span>
+            <div className="flex justify-center w-screen h-screen">
+                <ReactFlow
+                    nodes={initialNodes}
+                    edges={initialEdges}
+                    onNodeClick={handleNodeClick}
                 />
+            </div>
         </div>
     );
-}
+};
 export default PathComponent;
-
-// const navigate = useNavigate();
-
-//   type questionProps = {
-//     questionText: string;
-//     openEnded: boolean;
-//     category: string;
-//     saveToProfile: string;
-//     options: string[];
-//     correctOption: string[];
-//     _id: string;
-//   };
-
-//   const [chartData, setChartData] = useState([]);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await fetch(`https://localhost:3000/questions/${condition}`, {
-//           method: 'GET',
-//           headers: {
-//             'Content-Type': 'application/json',
-//           },
-//         });
-
-//         if (!response.ok) {
-//           throw new Error('Network response was not ok');
-//         }
-
-//         const data = await response.json();
-//         const mappedChartData = data.map((question: questionProps, index: number, array: any) => ({
-//           id: question._id,
-//           questionText: question.questionText,
-//           parent: index > 0 ? array[index - 1].id : '',
-//         }));
-        
-//         setChartData(mappedChartData);
-//       } catch (error) {
-//         console.error('Error fetching data:', error);
-//       }
-//     };
-
-//     fetchData();
-//   }, [condition]);
-
-// };
-//   const { condition } = useParams();
