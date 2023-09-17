409765b57f2594008e0e47b4c8014135524daf13
Steven-Jarmell <sjj27@pitt.edu>
2023-09-16 21:40:54 -0400

    Add clarity to homepage

--- client/src/components/HomeComponent.tsx
+++ client/src/components/HomeComponent.tsx
@@ -9,33 +9,35 @@ import covid from "../assets/covid.png";
 const options = [
     { name: "Asthma", path: "/asthma", imgSrc: asthma },
     { name: "Diabetes", path: "/diabetes", imgSrc: diabetes },
-    { name: "Stomachache", path: "/stomach", imgSrc: stomach },
+    { name: "IBS", path: "/stomach", imgSrc: stomach },
     { name: "Covid", path: "/covid", imgSrc: covid },
 ];
 
-
 const HomeComponent = () => {
-
     useEffect(() => {
         const getData = async () => {
-            const req = await fetch('http://localhost:3000/questions', {
+            const req = await fetch("http://localhost:3000/questions", {
                 method: "GET",
-                
-            }).then(res => res.json()).then((data) => console.log(data))
-            console.log(req)
-        }
+            })
+                .then((res) => res.json())
+                .then((data) => console.log(data));
+            console.log(req);
+        };
         getData();
-    }, [])
+    }, []);
     return (
-        <div className="flex justify-center mt-40 gap-32">
-            {options.map((option) => (
-                <OptionCard
-                    key={option.name}
-                    name={option.name}
-                    path={option.path}
-                    imgSrc={option.imgSrc}
-                />
-            ))}
+        <div className="flex flex-col justify-center h-fit">
+            <span className="text-center text-3xl w-full">Click a card to start learning about common illnesses!</span>
+            <div className="flex justify-center mt-24 gap-32">
+                {options.map((option) => (
+                    <OptionCard
+                        key={option.name}
+                        name={option.name}
+                        path={option.path}
+                        imgSrc={option.imgSrc}
+                    />
+                ))}
+            </div>
         </div>
     );
 };
